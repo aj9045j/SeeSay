34d8974ae08419ab6e2a28eeef783ed7f5e8f053
aj9045j <aj9045j@gmail.com>
2023-07-23 10:28:42 +0530

    added mobile restriction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './style/global.css';
-import JoinRoom from './pages/joinRoom';
-import WebRtc from './component/WebRtc';
-import { Toaster } from 'react-hot-toast'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style/global.css";
+import JoinRoom from "./pages/joinRoom";
+import WebRtc from "./component/WebRtc";
+import { Toaster } from "react-hot-toast";
+import MobileDetect from "mobile-detect";
 
 function App() {
+  
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    const md = new MobileDetect(window.navigator.userAgent);
+    setIsMobile(md.mobile() !== null);
+  }, []);
 
+  if (isMobile) {
+    return (
+      <div className="error-message">
+        <h1>Oops! Mobile Access Restricted</h1>
+        <p>We're sorry, but this website is not available on mobile devices.</p>
+      </div>
+    );
+  }
   return (
     <>
       <div>
--- frontend/src/component/WebRtc.jsx
+++ frontend/src/component/WebRtc.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import socketInit from '../socket-io/socket';
-import toast from 'react-hot-toast';
-import { FaEnvelope } from 'react-icons/fa';
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+import socketInit from "../socket-io/socket";
+import toast from "react-hot-toast";
+import { FaEnvelope } from "react-icons/fa";
 import { BsMicMute, BsMicFill } from "react-icons/bs";
 import { PiCameraRotateDuotone } from "react-icons/pi";
 import { FiCameraOff, FiCamera } from "react-icons/fi";
@@ -10,19 +10,18 @@ const connections = new Map();
 const mediaMap = new Map();
 
 function WebRtc() {
-
     const location = useLocation();
-    const [facingMode, setFacingMode] = useState('user');
+    const [facingMode, setFacingMode] = useState("user");
     const searchParams = new URLSearchParams(location.search);
-    const user = searchParams.get('userId');
-    const room = searchParams.get('roomId');
+    const user = searchParams.get("userId");
+    const room = searchParams.get("roomId");
     const myElementRef = useRef(null);
     const [stream, setStream] = useState(null);
     const [remoteTracks, setRemoteTracks] = useState(new Map());
     const [cameraEnabled, setCameraEnabled] = useState(true);
     const [audioEnabled, setAudioEnabled] = useState(true);
     const [show, setShow] = useState(false);
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
     const socketRef = useRef(null);
     const [newMessage, setNewMessage] = useState(false);
@@ -45,97 +44,101 @@ function WebRtc() {
         setMessage(inputMessage);
     };
     const handleSendMessage = () => {
-        if (message.trim() !== '') {
+        if (message.trim() !== "") {
             console.log(message);
-            socketRef.current.emit('sendMessage', {
+            socketRef.current.emit("sendMessage", {
                 message: message,
                 roomId: room,
-                userId: user
+                userId: user,
             });
 
-            setMessage('');
+            setMessage("");
         }
-    }
+    };
     const showMessage = () => {
         // setNewMessage(!newMessage);
         setShow(!show);
     };
 
-
     useEffect(() => {
-
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: facingMode } }, audio: true }).then(async streams => {
-
-            myElementRef.current.srcObject = streams;
-            toast.success("getting user media");
-            await setStream(streams);
-        }).catch((err) => {
-            toast.error("not getting media");
-        });
+        navigator.mediaDevices
+            .getUserMedia({
+                video: { facingMode: { exact: facingMode } },
+                audio: true,
+            })
+            .then(async (streams) => {
+                myElementRef.current.srcObject = streams;
+                toast.success("getting user media");
+                await setStream(streams);
+            })
+            .catch((err) => {
+                toast.error("not getting media");
+            });
     }, [facingMode]);
-
 
     const changeCamera = async () => {
         const devices = await navigator.mediaDevices.enumerateDevices();
-        const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
-        if (rearCamera) setFacingMode(prevMode => prevMode === 'environment' ? 'user' : 'environment');
-        toast.success("camera changed")
+        const rearCamera = devices.find(
+            (device) =>
+                device.kind === "videoinput" &&
+                device.label.toLowerCase().includes("back")
+        );
+        if (rearCamera)
+            setFacingMode((prevMode) =>
+                prevMode === "environment" ? "user" : "environment"
+            );
+        toast.success("camera changed");
     };
 
     const toggleCamera = () => {
-        setCameraEnabled(prevState => !prevState);
+        setCameraEnabled((prevState) => !prevState);
         if (stream) {
-            stream.getVideoTracks().forEach(track => {
+            stream.getVideoTracks().forEach((track) => {
                 track.enabled = !cameraEnabled;
             });
         }
         if (cameraEnabled) {
-            toast('camera off', {
-                icon: '📷',
+            toast("camera off", {
+                icon: "📷",
             });
-        }
-        else {
-            toast('camera on', {
-                icon: '📸',
+        } else {
+            toast("camera on", {
+                icon: "📸",
             });
         }
     };
     const toggleAudio = () => {
-        setAudioEnabled(prevState => !prevState);
+        setAudioEnabled((prevState) => !prevState);
         if (stream) {
-            stream.getAudioTracks().forEach(track => {
+            stream.getAudioTracks().forEach((track) => {
                 track.enabled = !audioEnabled;
             });
         }
         if (audioEnabled) {
-            toast('mic off', {
-                icon: '🎙️',
+            toast("mic off", {
+                icon: "🎙️",
+            });
+        } else {
+            toast("mic on", {
+                icon: "🎙️",
             });
         }
-        else {
-            toast('mic on', {
-                icon: '🎙️',
-            });
-        }
-    }
-
+    };
 
     const handleTrack = async ({ event, clientId }) => {
-
         mediaMap.set(clientId, event.streams[0].id);
-        await setRemoteTracks(prevRemoteTracks => new Map(prevRemoteTracks).set(event.streams[0].id, event.streams[0]));
-
+        await setRemoteTracks((prevRemoteTracks) =>
+            new Map(prevRemoteTracks).set(event.streams[0].id, event.streams[0])
+        );
     };
 
     const removeRemoteTrack = async (trackId) => {
-        setRemoteTracks(prevRemoteTracks => {
+        setRemoteTracks((prevRemoteTracks) => {
             const updatedRemoteTracks = new Map(prevRemoteTracks);
             updatedRemoteTracks.delete(trackId);
             return updatedRemoteTracks;
         });
-
     };
-
 
     const startConnection = async () => {
         toast.success("connecting....");
@@ -146,7 +149,7 @@ function WebRtc() {
 
         console.log(socket);
         socketRef.current = socket;
-        socket?.on('message', (message) => {
+        socket?.on("message", (message) => {
             toast.success("new message");
             if (!show) setNewMessage(true);
             console.log(message);
@@ -154,105 +157,96 @@ function WebRtc() {
             setMessages((prevMessages) => [...prevMessages, message]);
         });
         if (stream) {
-
-            socket?.on('newUser', () => {
-                socket.emit('joinUser', {
+            socket?.on("newUser", () => {
+                socket.emit("joinUser", {
                     roomId: room,
-                    userId: user
+                    userId: user,
                 });
-
             });
 
-            socket?.on('createOffer', async ({ roomId, clientId, socketId }) => {
-
-
+            socket?.on("createOffer", async ({ roomId, clientId, socketId }) => {
                 let peer = await new RTCPeerConnection({
                     iceServers: [
                         {
                             urls: [
-                                'stun:stun.l.google.com:19302',
-                                'stun:global.stun.twilio.com:3478'
-                            ]
-                        }
-                    ]
+                                "stun:stun.l.google.com:19302",
+                                "stun:global.stun.twilio.com:3478",
+                            ],
+                        },
+                    ],
                 });
 
-                peer.ontrack = event => {
+                peer.ontrack = (event) => {
                     handleTrack({
                         event: event,
-                        clientId: clientId
+                        clientId: clientId,
                     });
                 };
 
-                peer.onicecandidate = event => {
-
+                peer.onicecandidate = (event) => {
                     if (event.candidate) {
-                        socket.emit('sendIceCandidate', {
+                        socket.emit("sendIceCandidate", {
                             roomId: room,
                             clientId: clientId,
-                            candidate: event.candidate
+                            candidate: event.candidate,
                         });
                     }
-                }
+                };
 
                 if (stream) {
-                    await stream.getTracks().forEach(track => {
+                    await stream.getTracks().forEach((track) => {
                         peer.addTrack(track, stream);
                     });
                 } else {
-                    console.warn('stream not found');
+                    console.warn("stream not found");
                 }
 
                 await connections.set(clientId, peer);
                 let offer = await peer.createOffer();
                 peer.setLocalDescription(offer);
 
-                socket.emit('sendOffer', {
+                socket.emit("sendOffer", {
                     clientId: clientId,
                     offer: offer,
-                    socketId: socketId
+                    socketId: socketId,
                 });
-
-
             });
 
-            socket?.on('createAns', async ({ clientId, offer, socketId }) => {
-
+            socket?.on("createAns", async ({ clientId, offer, socketId }) => {
                 let peer = await new RTCPeerConnection({
                     iceServers: [
                         {
                             urls: [
-                                'stun:stun.l.google.com:19302',
-                                'stun:global.stun.twilio.com:3478'
-                            ]
-                        }
-                    ]
+                                "stun:stun.l.google.com:19302",
+                                "stun:global.stun.twilio.com:3478",
+                            ],
+                        },
+                    ],
                 });
 
-                peer.ontrack = event => {
+                peer.ontrack = (event) => {
                     handleTrack({
                         event: event,
-                        clientId: socketId
+                        clientId: socketId,
                     });
                 };
 
-                peer.onicecandidate = event => {
-
+                peer.onicecandidate = (event) => {
                     if (event.candidate) {
-                        socket.emit('sendIceCandidate', {
+                        socket.emit("sendIceCandidate", {
                             roomId: room,
                             clientId: clientId,
-                            candidate: event.candidate
+                            candidate: event.candidate,
                         });
                     }
-                }
+                };
                 connections.set(socketId, peer);
                 if (stream) {
-                    stream.getTracks().forEach(track => {
+                    stream.getTracks().forEach((track) => {
                         peer.addTrack(track, stream);
                     });
                 } else {
-                    console.warn('stream not found');
+                    console.warn("stream not found");
                 }
 
                 await peer.setRemoteDescription(offer);
@@ -260,74 +254,63 @@ function WebRtc() {
 
                 await peer.setLocalDescription(ans);
 
-                toast.success("new user joined")
+                toast.success("new user joined");
 
-                socket.emit('sendAns', {
+                socket.emit("sendAns", {
                     clientId: clientId,
                     ans: ans,
-                    socketId: socketId
+                    socketId: socketId,
                 });
 
-                peer.onnegotiationneeded = async event => {
-
-                    socket.emit('nego', {
+                peer.onnegotiationneeded = async (event) => {
+                    socket.emit("nego", {
                         clientId: clientId,
-                        socketId: socketId
+                        socketId: socketId,
                     });
                 };
-
             });
 
-            socket?.on('setAns', async ({ clientId, ans, socketId }) => {
-
+            socket?.on("setAns", async ({ clientId, ans, socketId }) => {
                 let peer = connections.get(clientId);
                 await peer.setRemoteDescription(ans);
-
             });
 
-            socket?.on('nego:coffer', async ({ socketId, clientId }) => {
-
+            socket?.on("nego:coffer", async ({ socketId, clientId }) => {
                 let peer = connections.get(clientId);
                 let offer = await peer.createOffer();
                 await peer.setLocalDescription(offer);
-                socket.emit('nego:soffer', {
+                socket.emit("nego:soffer", {
                     offer: offer,
                     socketId: socketId,
-                    clientId: clientId
+                    clientId: clientId,
                 });
             });
 
-            socket?.on('nego:cans', async ({ socketId, offer, clientId }) => {
-
+            socket?.on("nego:cans", async ({ socketId, offer, clientId }) => {
                 let peer = connections.get(socketId);
                 await peer.setRemoteDescription(offer);
                 let ans = await peer.createAnswer(offer);
-                socket.emit('nego:sans', {
+                socket.emit("nego:sans", {
                     socketId: socketId,
                     ans: ans,
-                    clientId: clientId
+                    clientId: clientId,
                 });
             });
 
-            socket?.on('nego:setlocal', async ({ socketId, ans, clientId }) => {
-
+            socket?.on("nego:setlocal", async ({ socketId, ans, clientId }) => {
                 let peer = connections.get(clientId);
                 await peer.setRemoteDescription(ans);
-
             });
 
-            socket?.on('receiveIceCandidate', async ({ clientId, candidate }) => {
+            socket?.on("receiveIceCandidate", async ({ clientId, candidate }) => {
                 candidate.usernameFragment = null;
                 let peer = await connections.get(clientId);
 
                 if (peer) {
-
                     await peer.addIceCandidate(candidate);
-
                 } else {
-                    console.log('Peer not found');
+                    console.log("Peer not found");
                 }
-
             });
 
             socket?.on("removeUser", async ({ socketId }) => {
@@ -337,23 +320,13 @@ function WebRtc() {
                 await removeRemoteTrack(mediaMap.get(socketId));
                 mediaMap.delete(socketId);
                 console.log("media map deleted", mediaMap);
-            })
-
-
-
+            });
         }
-
-
-
-
-
-
     };
 
     return (
         <>
             <div onMouseMove={handleMouseMove}>
-
                 <div className="inbox-container">
                     <div className="inbox-icon" onClick={showMessage}>
                         <FaEnvelope />
@@ -383,7 +356,6 @@ function WebRtc() {
                     )}
                 </div>
 
-
                 {showMenu && (
                     <div className="modify">
                         <button onClick={changeCamera}>
@@ -394,13 +366,11 @@ function WebRtc() {
                         </button>
                         <button onClick={toggleAudio}>
                             {audioEnabled ? <BsMicMute /> : <BsMicFill />}
-
                         </button>
-
                     </div>
                 )}
 
-                <div className='flex'>
+                <div className="flex">
                     <div className="video-container">
                         <video
                             src=""
@@ -408,43 +378,43 @@ function WebRtc() {
                             ref={myElementRef}
                             autoPlay
                             id="localstream"
-                            style={{ borderRadius: '55px', margin: "5px" }}
+                            style={{ borderRadius: "55px", margin: "5px" }}
                             height="300px"
-
-
                         ></video>
-
                     </div>
 
-                    {
-                        [...remoteTracks.values()].map((track, index) => {
-                            console.log(remoteTracks);
-                            return (
-                                <div className="video-container">
-                                    <video
-                                        key={index}
-                                        ref={video => {
-                                            if (video) {
-                                                video.srcObject = track;
-                                            }
-                                        }}
-                                        playsInline
-                                        autoPlay
-                                        style={{ borderRadius: '55px', margin: "5px" }}
-                                        height="300px"
-                                        max-height="300px"
-                                    // width="auto"
-                                    ></video>
-                                </div>
-                            );
-                        })
-                    }
+                    {[...remoteTracks.values()].map((track, index) => {
+                        console.log(remoteTracks);
+                        return (
+                            <div className="video-container">
+                                <video
+                                    key={index}
+                                    ref={(video) => {
+                                        if (video) {
+                                            video.srcObject = track;
+                                        }
+                                    }}
+                                    playsInline
+                                    autoPlay
+                                    style={{ borderRadius: "55px", margin: "5px" }}
+                                    height="300px"
+                                    max-height="300px"
+                                // width="auto"
+                                ></video>
+                            </div>
+                        );
+                    })}
+                </div>
 
-                </div >
-
-                {showButton && <button style={{ position: "absolute", bottom: "17%", left: "47%" }} onClick={startConnection}>JOIN</button>}
+                {showButton && (
+                    <button
+                        style={{ position: "absolute", bottom: "17%", left: "47%" }}
+                        onClick={startConnection}
+                    >
+                        JOIN
+                    </button>
+                )}
             </div>
-
         </>
     );
 }
